e19d20a365321bdc6da5ee26e3ccc2f43420a531
Prathamesh Chaughule <166697280+PrathameshChaughule@users.noreply.github.com>
2025-02-01 18:37:19 +0530

    first commit

---
+++ Frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favorite food here</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At maxime modi temporibus dolor aperiam deleniti totam aspernatur aut suscipit atque sapiente repellat corrupti hic, reiciendis tempore optio doloremque ut accusantium.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
